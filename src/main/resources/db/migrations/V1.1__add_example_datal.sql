d6d26e16d7293ce5841b3bc9bd547e26869d2114
Eldar Erel <eldar.erel@gmail.com>
2022-11-19 16:16:24 +0200

    fix: example file

--- src/main/resources/db/migrations/V1.1__add_example_datal.sql
+++ src/main/resources/db/migrations/V1.1__add_example_datal.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.file(id, content, date_added, date_modified, kind, name, size)
-VALUES ('c5490be3-926c-4d27-b6f9-06e8d5057504', 'IyBTaGFyZWQgRm9sZGVyIEV4YW1wbGUgZmlsZQ==',
+VALUES ('c5490be3-926c-4d27-b6f9-06e8d5057504', '# Shared Folder Example file',
         '2022-10-22T10:55:03.075870Z', '2022-10-22T10:55:03.075870Z', 'md', 'EXAMPLE.md', '301 bytes');
